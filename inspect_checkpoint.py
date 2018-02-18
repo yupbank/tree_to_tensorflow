c66fb8c2a7775d4fffe2f800dec3face01b816d3
Peng Yu <peng.yu@shopify.com>
2018-02-18 10:15:00 -0500

    aaa

--- inspect_checkpoint.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.python import pywrap_tensorflow
-
-reader = pywrap_tensorflow.NewCheckpointReader('./tmp/model.ckpt-211')
-
-print reader.get_variable_to_shape_map()
-import ipdb; ipdb.set_trace()
