c3f7d141ab0350f365882649b468fd9683f127fd
Peng YU <peng.yu@shopify.com>
2019-12-11 17:34:44 -0500

    remove shit

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,42 +4,6 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import numpy as np
 import pytest
 
-import ttt.weight_utils as wutils
-
-
-@pytest.fixture
-def weight():
-    root = wutils.binary_node(node_id=0, feature_id=0,
-                              threshold=0, left_child_id=1, right_child_id=2)
-    left = wutils.leaf_node(node_id=1, value=[0.0, 1.0])
-    right = wutils.leaf_node(node_id=2, value=[1.0, 0.0])
-    return wutils.tree_model([root, left, right])
-
-
-@pytest.fixture
-def stat():
-    return wutils.fertile_stat([wutils.node_stat(2, 1), wutils.node_stat(1, 1)])
-
-
-@pytest.fixture
-def postive_x():
-    return [-1]
-
-
-@pytest.fixture
-def pred_positive():
-    return [{'floatValue': 0.0}, {'floatValue': 1.0}]
-
-
-@pytest.fixture
-def negative_x():
-    return [1]
-
-
-@pytest.fixture
-def pred_negative():
-    return [{'floatValue': 1.0}, {'floatValue': 0.0}]
-
 
 @pytest.fixture
 def dataset():
@@ -47,13 +11,6 @@ def dataset():
                                n_informative=2, n_redundant=0, random_state=0, shuffle=False)
     X = np.asarray(X, dtype=np.float32)
     return X, y
-
-
-@pytest.fixture
-def clf(dataset):
-    clf = RandomForestClassifier()
-    clf.fit(*dataset)
-    return clf
 
 
 @pytest.fixture(params=[DecisionTreeClassifier(), RandomForestClassifier(n_estimators=4), DecisionTreeRegressor(), RandomForestRegressor(n_estimators=4)],
--- tests/test_sklearn_helper.py
+++
@@ -1,4 +0,0 @@
-
-
-def test_extract_weights(clf):
-    return
