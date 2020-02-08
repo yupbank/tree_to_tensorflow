65e283a505333ba9ab5e5d9f34dfe8f1fea22d12
Peng YU <peng.yu@shopify.com>
2020-02-08 11:33:40 +0100

    add 3 class

--- tests/test_tf_xgb_inference.py
+++ tests/test_tf_xgb_inference.py
@@ -15,8 +15,17 @@ def test_regression_inference():
           sess.run(a.predict(x)), model.predict(x), decimal=3)
 
 
-def test_classification_inference():
+def test_classification_inference_2_class():
     x, y = make_classification(random_state=10)
+    model = xgb.XGBClassifier(random_state=10).fit(x, y)
+    b = TreeClassificationInference(model)
+    with tf.Session() as sess:
+      np.testing.assert_array_almost_equal(
+          sess.run(b.predict_proba(x)), model.predict_proba(x), decimal=3)
+
+
+def test_classification_inference_3_class():
+    x, y = make_classification(random_state=10, n_classes=3, n_informative=10)
     model = xgb.XGBClassifier(random_state=10).fit(x, y)
     b = TreeClassificationInference(model)
     with tf.Session() as sess:
--- ttt/tf_xgb_inference.py
+++ ttt/tf_xgb_inference.py
@@ -51,12 +51,14 @@ class TreeClassificationInference(TreeRegressionInference):
 
     def predict_proba(self, input_):
         predicted = self._predict(input_)
-        if self.clf.n_classes_ > 2:
-            predicted = tf.reshape(predicted, (-1, self.clf.n_classes_))
-            predicted = tf.softmax(predicted, axis=1)
-        positive_pred = tf.sigmoid(tf.reduce_sum(predicted, axis=0))[
-            :, tf.newaxis]
-        return tf.concat([1-positive_pred, positive_pred], axis=1)
+        if self.clf.n_classes_ <= 2:
+            positive_pred = tf.sigmoid(tf.reduce_sum(predicted, axis=0))[
+                :, tf.newaxis]
+            return tf.concat([1-positive_pred, positive_pred], axis=1)
+        else:
+            predicted = tf.reduce_sum(tf.stack(
+                tf.split(predicted, self.clf.n_classes_, axis=0), axis=2), axis=0)
+            return tf.nn.softmax(predicted, axis=1)
 
     def predict(self, input_):
         prob = self.predict_proba(input_)
