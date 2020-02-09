b7926a5dd8d3772af66636139d2606f1bbb71837
Peng YU <peng.yu@shopify.com>
2020-02-09 13:54:01 +0100

    boost

--- tests/test_xgb_inference.py
+++ tests/test_xgb_inference.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 import xgboost as xgb
 from sklearn.datasets import make_regression, make_classification
 
-from ttt.xgb_inference import TreeRegressionInference, TreeClassificationInference
+from ttt.xgb_inference import RegressionInference, ClassificationInference
 
 
 def test_regression_inference():
     x, y = make_regression(random_state=10)
     model = xgb.XGBRegressor(random_state=10).fit(x, y)
-    a = TreeRegressionInference(model)
+    a = RegressionInference(model)
     with tf.Session() as sess:
       np.testing.assert_array_almost_equal(
           sess.run(a.predict(x)), model.predict(x), decimal=3)
@@ -18,7 +18,7 @@ def test_regression_inference():
 def test_classification_inference_2_class(binary_classification_dataset):
     x, y = binary_classification_dataset
     model = xgb.XGBClassifier(random_state=10).fit(x, y)
-    b = TreeClassificationInference(model)
+    b = ClassificationInference(model)
     with tf.Session() as sess:
       np.testing.assert_array_almost_equal(
           sess.run(b.predict_proba(x)), model.predict_proba(x), decimal=3)
@@ -29,7 +29,7 @@ def test_classification_inference_2_class(binary_classification_dataset):
 def test_classification_inference_3_class(multiclass_classification_dataset):
     x, y = multiclass_classification_dataset
     model = xgb.XGBClassifier(random_state=10).fit(x, y)
-    b = TreeClassificationInference(model)
+    b = ClassificationInference(model)
     with tf.Session() as sess:
       np.testing.assert_array_almost_equal(
           sess.run(b.predict_proba(x)), model.predict_proba(x), decimal=3)
